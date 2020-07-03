83742fe68d480c281e0fd1d60b403d72d3b33f00
Lewis Able Rafuse <rafusel@mcmaster.ca>
2020-07-03 02:17:59 -0400

    Updates

---
+++ precache-manifest.f318c7353564f9a14d43f88e7564fc00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61812fedfd7025125b51d68684f7969",
+    "url": "/dividend-tracker-ui/index.html"
+  },
+  {
+    "revision": "f44e4324b5d1dbf89b22",
+    "url": "/dividend-tracker-ui/static/css/main.fa6ba895.chunk.css"
+  },
+  {
+    "revision": "bd54c2f0255eda67fc85",
+    "url": "/dividend-tracker-ui/static/js/2.f95b8a74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dividend-tracker-ui/static/js/2.f95b8a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44e4324b5d1dbf89b22",
+    "url": "/dividend-tracker-ui/static/js/main.d33b948c.chunk.js"
+  },
+  {
+    "revision": "f8a9f9a226fdfe0e3e27",
+    "url": "/dividend-tracker-ui/static/js/runtime-main.1f837387.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/dividend-tracker-ui/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "0c226a4abfe398c4f64bae682c46f2ee",
+    "url": "/dividend-tracker-ui/static/media/wealthsimple-pink.0c226a4a.png"
+  }
+]);
